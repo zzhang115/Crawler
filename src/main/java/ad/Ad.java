0c659f4e95dad14983f29ddf53389c76af215527
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-10 23:01:43 -0700

    add jar files

---
+++ src/main/java/ad/Ad.java
@@ -0,0 +1,7 @@
+package ad;
+
+/**
+ * Created by zzc on 8/10/17.
+ */
+public class Ad {
+}
---
+++ src/main/java/crawler/CrawlerForAmazon.java
@@ -0,0 +1,7 @@
+package crawler;
+
+/**
+ * Created by zzc on 8/10/17.
+ */
+public class CrawlerForAmazon {
+}
