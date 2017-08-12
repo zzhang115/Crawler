a42f4ce466f0eff02803bf963c45acf9732fcea7
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-11 20:35:26 -0700

    add crawler codes

--- src/main/java/crawler/CrawlerForAmazon.java
+++ src/main/java/crawler/CrawlerForAmazon.java
@@ -1,7 +1,53 @@
 package crawler;
 
+import java.io.*;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zzc on 8/10/17.
  */
+
 public class CrawlerForAmazon {
+    private static final String AMAZON_QUERY_URL = "https://www.amazon.com/s/ref=nb_sb_noss?field-keywords=";
+    private static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36";
+    private final String authUser = "ganjiayan";
+    private final String authPassword = "Bi7jU9TI";
+    private List<String> proxyList;
+    private List<String> titleList;
+    private List<String> categoryList;
+    private BufferedWriter logBufferWriter;
+    private int index = 0;
+
+    public CrawlerForAmazon(String proxy_file, String log_file) {
+        initProxyIPList(proxy_file);
+    }
+
+    private void initProxyIPList(String proxyfile) {
+        proxyList = new ArrayList<String>();
+
+        try {
+            String line;
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(proxyfile));
+            while((line = bufferedReader.readLine()) != null) {
+                String[] proxy = line.split(",");
+                String ip = proxy[0].trim();
+                System.out.println(ip);
+                proxyList.add(ip);
+            }
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(authUser, authPassword.toCharArray());
+            }
+        });
+    }
 }
--- src/main/java/crawler/Main.java
+++ src/main/java/crawler/Main.java
@@ -1,7 +1,22 @@
 package crawler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Created by zzc on 8/10/17.
  */
 public class Main {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: Crawler <rawQueryDataFilePath> " +
+                    "<adsDataFilePath> <proxyFilePath> <logFilePath>");
+            System.exit(0);
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        String rawQueryDataFilePath = args[0];
+        String adsDataFilePath = args[1];
+        String proxyFilePath = args[2];
+        String logFilePath = args[3];
+        CrawlerForAmazon crawler = new CrawlerForAmazon(proxyFilePath, logFilePath);
+    }
 }
